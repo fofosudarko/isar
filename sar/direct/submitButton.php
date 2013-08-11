c5283cbc882ea612c9c0734516763a1711611b88
Frederick Ofosu-Darko <code.ricks@gmail.com>
2013-08-11 11:50:14 +0000

    edited submitButton.php

--- sar/direct/submitButton.php
+++ sar/direct/submitButton.php
@@ -5,7 +5,7 @@
 function submitButton($faceText="GO&raquo;", $style="", $pos="absolute", $submit="") {
 	$submitButton = <<<EOF
 	<div style='$style position: $pos;' class="sub-button-position">
-	<input name="$submit" type="submit" class="inline-button-style"  id="submit-button" value="$faceText">
+	<input name="$submit" type="submit" class="inline-button-style" id="submit-button" value="$faceText">
 	</div>
 EOF;
 
